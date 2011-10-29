31986a2837be4e601abe63952692fd2e6ece07ab
Jinho Chung by Macbook Pro <pearlc11@gmail.com>
2011-10-29 15:55:02 +0900

    commit test from 15"MacbookPro

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -8,7 +8,7 @@ class Blog extends CI_Controller {
 
 	function index() {
 		$data["title"] = "My Blog Title";
-		$data["header_message"] = "Testt Header Message";
+		$data["header_message"] = "Header Message";
 		$data["footer_message"] = "Footer Message";
 
 		$data["main_content"] = "";
