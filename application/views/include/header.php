20aa923591b48fa61a5d492c329cc9da8920a31b
jinho Chung <pearlc11@gmail.com>
2011-10-30 11:39:26 +0900

    image

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -3,6 +3,11 @@
 <html>
 <head>
 	<title><?=$title?></title>
+	 <!-- Framework CSS -->
+	<base href="<?=base_url();?>">
+    <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="css/print.css" type="text/css" media="print">
+    <!--[if lt IE 8]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen, projection"><![endif]-->
 </head>
 <body>
 	<h1><?=$header_message?></h1>
